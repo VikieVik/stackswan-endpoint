9e8dd7d1f349eab4e1d6e1f05c4d86f3d907662e
Vikas Singh <vikas3091999@gmail.com>
2020-12-12 17:32:29 +0530

    Update stackswan-endpoint.ino

--- stackswan-endpoint.ino
+++ stackswan-endpoint.ino
@@ -1,80 +1,114 @@
- 
-/**
- * ==================================================================
- * Stackswan :
- * Uses CDP to communicate over LoRa ad-hoc
- * MAMA is upated to send data over clusterduck network
- * 
- * TODO: 
- * 1) Edit captive portal with stackswan branding 
- * 2) Add a method for sending test sensor data 
- * ==================================================================
- */
-
-#include "timer.h"
-#include <MamaDuck.h>
-#include <DuckDisplay.h>
-
-auto timer = timer_create_default();
-const int INTERVAL_MS = 50000;
-char message[32]; 
-int counter = 1;
-
-const char* DUCK_WIFI_AP = "MAMA DUCK PORTAL";
-// create an instance of a MamaDuck with a given unique id
-
-String deviceId = "MAMA001";
-
-  
-MamaDuck duck = MamaDuck(deviceId);
-DuckDisplay* display = NULL;
+/*
+ =================================================================================
+  Stackswan LoRa p2p - (endpoint)
+ ---------------------------------------------------------------------------------
+  1) Endpoint can use any unique 2byte address otherthan 0xFF.
+  2) 0xFF is reserved for all gateway devices in the network
+  3) Sends a message every half second, and polls for incoming messages continually
+  4) if message is for this device then process message otherwise retransmit.
+  5) For easy star networking all non-gateway devices(endpoints) sends message
+     to gateway using 0xFF as target.
+  ---------------------------------------------------------------------------------   
+  Author: Vikas Singh 
+  Date: 12/12/20
+  ==================================================================================
+*/
 
 
-// LORA RF CONFIG
-#define LORA_FREQ 868.0 // Frequency Range. Set for US Region 915.0Mhz
-#define LORA_TXPOWER 20 // Transmit Power
-// LORA HELTEC PIN CONFIG
-#define LORA_CS_PIN 5
-#define LORA_DIO0_PIN 2
-#define LORA_DIO1_PIN -1 // unused
-#define LORA_RST_PIN 14
+#include <SPI.h>              
+#include <LoRa.h>             
 
+const int csPin = 5;          // LoRa radio chip select
+const int resetPin = 14;      // LoRa radio reset
+const int irqPin = 2;         // change for your board; must be a hardware interrupt pin
+
+String outgoing;              // outgoing message
+
+byte msgCount = 0;            // count of outgoing messages
+byte localAddress = 0xAA;     // address of this device(unique for each device)
+byte destination = 0xFF;      // destination to send to(0xFF for all border routers)
+long lastSendTime = 0;        // last send time
+int interval = 2000;          // initial interval between sends
 
 void setup() {
-  // initialize the serial component with the hardware supported baudrate
-  duck.setupSerial(115200);
-  // initialize the LoRa radio with specific settings. This will overwrites settings defined in the CDP config file cdpcfg.h
-  duck.setupRadio(LORA_FREQ, LORA_CS_PIN, LORA_RST_PIN, LORA_DIO0_PIN, LORA_DIO1_PIN, LORA_TXPOWER);
-  // initialize the wifi access point with a custom AP name
-  duck.setupWifi(DUCK_WIFI_AP);
-  // initialize DNS
-  duck.setupDns();
-  // initialize web server, enabling the captive portal with a custom HTML page
-//  duck.setupWebServer(true, HTML);
-  duck.setupWebServer(true);
+  
+  // initialize serial
+  Serial.begin(115200);                   
+  while (!Serial);
+  Serial.println("stackswan P2P");
+  
+  // override the default CS, reset, and IRQ pins (optional)
+  // set CS, reset, IRQ pin
+  LoRa.setPins(csPin, resetPin, irqPin);
 
-  // initialize Over The Air firmware upgrade
-  duck.setupOTA();
-  timer.every(INTERVAL_MS, runSensor);
+  // initialize radio at 866MHz (eg: 915E6, 868E6 etc)
+  if (!LoRa.begin(866E6)) {             
+    Serial.println("LoRa initialization failed. Check your connections.");
+    while (true);                       // if failed, do nothing
+  }
 
-
-
+  Serial.println("LoRa initialization succeeded.");
 }
 
-void loop(){
-      timer.tick();
-  // Use the default run(). The Mama duck is designed to also forward data it receives
-  // from other ducks, across the network. It has a basic routing mechanism built-in
-  // to prevent messages from hoping endlessly.
-  duck.run();
-    
-    
-    };
+void loop() {
 
-  bool runSensor(void *) {
-  sprintf(message, "mama counter %d", counter++); 
-  Serial.print(message);
-  duck.sendPayloadStandard(message, "counter-message"); // sender id will be populated automatically
-  
-  return true;
+  // This is only for endpoint devices
+  if (millis() - lastSendTime > interval) {
+    String message = "Hello from stackswan !";   //send a message
+    sendMessage(message);
+    Serial.println("Sending " + message);
+    lastSendTime = millis();                     //timestamp the message
+    interval = random(2000) + 1000;              //2-3 seconds
+  }
+
+  // parse for a packet, and call onReceive with the result:
+  onReceive(LoRa.parsePacket());
+}
+
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();                   // start packet
+  LoRa.write(destination);              // add destination address
+  LoRa.write(localAddress);             // add sender address
+  LoRa.write(msgCount);                 // add message ID
+  LoRa.write(outgoing.length());        // add payload length
+  LoRa.print(outgoing);                 // add payload
+  LoRa.endPacket();                     // finish packet and send it
+  msgCount++;                           // increment message ID
+}
+
+void onReceive(int packetSize) {
+  if (packetSize == 0) return;          // if there's no packet, return
+
+  // read packet header bytes:
+  int recipient = LoRa.read();          // recipient address
+  byte sender = LoRa.read();            // sender address
+  byte incomingMsgId = LoRa.read();     // incoming msg ID
+  byte incomingLength = LoRa.read();    // incoming msg length
+
+  String incoming = "";
+
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
+  }
+
+  if (incomingLength != incoming.length()) {                        // check length for error
+    Serial.println("error: data in the received payload is corrupted");
+    return;                                                         // skip rest of function
+  }
+
+  // if the recipient isn't this device or broadcast,
+  if (recipient != localAddress && recipient != 0xFF) {
+    Serial.println("This message is not for me.");
+    return;                                                   // skip rest of function
+  }
+
+  // if message is for this device, or broadcast, print details:
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Message length: " + String(incomingLength));
+  Serial.println("Message: " + incoming);
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
 }
